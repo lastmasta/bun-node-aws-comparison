bf88fe4ecae67ce604c15bbab3677bfc16d5fb56
Rodrigo Pineda <dubmasta91@gmail.com>
2025-05-19 22:41:53 -0600

    Add benchmarking to dijkstra-solver and image-resizer fargate services

---
+++ benchmarking/dijkstra-solver/node-fargate/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const autocannon = require('autocannon');
+
+const body = JSON.stringify(require('../body.json'));
+
+const instance = autocannon({
+  url: 'https://vnlo75rka7.execute-api.us-east-2.amazonaws.com/dev/dijkstra-solver/',
+  method: 'POST',
+  headers: {
+    'content-type': 'application/json',
+  },
+  body: body,
+  connections: 5, // 5 concurrent connections
+  duration: 60,
+});
+
+// This will print the results when complete
+autocannon.track(instance);
+
+instance.on('done', results => {
+  console.log(results);
+
+  // Save results to a file
+  const timestamp = new Date().toISOString().replace(/:/g, '-');
+  const resultsFilePath = path.join(
+    __dirname,
+    `benchmark-results-${timestamp}.json`
+  );
+
+  fs.writeFileSync(resultsFilePath, JSON.stringify(results, null, 2), 'utf8');
+
+  console.log(`Results saved to: ${resultsFilePath}`);
+});
